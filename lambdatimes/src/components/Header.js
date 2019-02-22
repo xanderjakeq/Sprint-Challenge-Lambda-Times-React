0e278dd143342716b68f03f63cafe4ac8ac52262
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-02-22 13:22:16 -0500

    MVP done

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import Styled from 'styled-components'
+import {ContainerDiv} from './TopBar'
+
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const Span = Styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  ${props => (
+    props.type === 'date' ? `
+      margin-left: 25px;
+      flex: 1;
+    ` : null
+  )}
+  
+  ${props => (
+    props.type === 'temp' ? `
+      text-align: right;
+      margin-right: 25px;
+      flex: 1;
+    ` : null
+  )}
+`
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <ContainerDiv type ="header">
+      <Span type="date">SMARCH 32, 2018</Span>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <Span type="temp">98°</Span>
+    </ContainerDiv>
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,96 @@
 import React from 'react';
-
+import Styled from 'styled-components'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const ContainerDiv = Styled.div`
+  ${props => (props.type === 'top-bar' ? `width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  ` : null )}
+  ${props => (
+    props.type === 'container' ? `
+      width: 100%;
+      display: flex;
+      justify-content: none;
+      align-items: none;
+      flex-direction: row;
+      color: #fff;
+      letter-spacing: 1px;
+      padding: 0 10px;
+      @media(min-width: 1280px){
+        width: 1280px
+      }     
+    ` : null
+  )}
+  ${props => (
+      props.type === 'container-left' ? `
+        display: flex;
+        justify-content: none;
+        align-items: center;
+        flex-direction: row;
+        flex: 1;
+        font-size: 11px;     
+      ` : null
+    )}
+  ${props => (
+      props.type === 'container-center' ? `
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        flex: 3;
+        font-size: 9px;
+      ` : null
+    )}
+  ${props => (
+    props.type === 'contianer-right' ? `
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: row;
+      flex: 3;
+      font-size: 9px;   
+    ` : null
+  )}
+  ${props => (
+      props.type === 'header' ? `
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: row;
+        background-color: #fff;
+        border-bottom: 1px solid lightgrey;
+        width: 100%;
+        height: 120px;
+        margin-top: 44px;
+        padding-bottom: 15px;
+      ` : null
+    )}
+`
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <ContainerDiv type = 'top-bar'>
+      <ContainerDiv type="container">
+        <ContainerDiv type ="container-left">
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </ContainerDiv>
+        <ContainerDiv type ="container-center">
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </ContainerDiv>
+        <ContainerDiv type ="container-right">
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </ContainerDiv>
+      </ContainerDiv>
+    </ContainerDiv>
   )
 }
 
 export default TopBar;
+export {ContainerDiv}

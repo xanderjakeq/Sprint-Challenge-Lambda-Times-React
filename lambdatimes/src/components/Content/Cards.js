28e9a9c9db64d7ce020e359359204450cbec6e0b
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-02-22 12:36:44 -0500

    Filtering cards Done

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import Card from './Card';
 
 const Cards = props => {
@@ -6,10 +7,19 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      {props.cards.map(card => <Card card = {card}/>)}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.shape({
+    headline: PropTypes.string,
+    tab: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string,
+  }))
+}
 
 export default Cards;
